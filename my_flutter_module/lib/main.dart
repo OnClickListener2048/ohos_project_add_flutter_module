7677d046aaa9e29b5eead840744977e569429891
OnClickListener2048 <wang29327713@gmail.com>
2025-10-08 15:34:22 +0800

    first commit

---
+++ my_flutter_module/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boost/flutter_boost.dart';
+
+void main() {
+  ///这里的CustomFlutterBinding调用务必不可缺少，用于控制Boost状态的resume和pause
+  print("CustomFlutterBinding start");
+  CustomFlutterBinding();
+  print("CustomFlutterBinding end");
+  runApp(MyApp());
+  print("runApp(MyApp()) end");
+}
+
+
+///创建一个自定义的Binding，继承和with的关系如下，里面什么都不用写
+class CustomFlutterBinding extends WidgetsFlutterBinding with BoostFlutterBinding {}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
+  /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
+  /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
+  /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
+  ///
+  /// 注意，如果需要push的时候，两个页面都需要动的话，
+  /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
+  /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
+  /// 简单来说，就两个页面都是CupertinoPageRoute就好
+  /// 如果用MaterialPageRoute的话同理
+
+  Map<String, FlutterBoostRouteFactory> routerMap = {
+    '/': (settings,bool isContainer, uniqueId) {
+      return CupertinoPageRoute(
+          settings: settings,
+          builder: (_) {
+            return Container();
+          });
+    },
+    'mainPage': (settings,bool isContainer, uniqueId) {
+      return CupertinoPageRoute(
+          settings: settings,
+          builder: (_) {
+            return MainPage(
+            );
+          });
+    },
+    'simplePage': (settings,bool isContainer, uniqueId) {
+      return CupertinoPageRoute(
+          settings: settings,
+          builder: (_) {
+
+            return SimplePage(
+
+            );
+          });
+    },
+  };
+
+  Route<dynamic>? routeFactory(RouteSettings settings, bool isInitialRoute, String? uniqueId) {
+    print("FlutterBoostRouteFactory start");
+    FlutterBoostRouteFactory func = routerMap[settings.name] as FlutterBoostRouteFactory;
+    print("FlutterBoostRouteFactory end");
+    return func(settings,isInitialRoute, uniqueId);
+  }
+
+  Widget appBuilder(Widget home) {
+    print("appBuilder");
+    return MaterialApp(
+      home: home,
+      debugShowCheckedModeBanner: true,
+
+      ///必须加上builder参数，否则showDialog等会出问题
+      builder: (_, __) {
+        return home;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("FlutterBoostApp1");
+    return FlutterBoostApp(
+      routeFactory,
+      appBuilder: appBuilder,
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage();
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Main Page')),
+    );
+  }
+}
+
+class SimplePage extends StatelessWidget {
+  const SimplePage();
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:  Center(child: Text('SimplePage')),
+    );
+  }
+}
